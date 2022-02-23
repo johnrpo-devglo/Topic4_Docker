02594b21d43bf72cbf42570a49bfde942ec4db52
johnrpo <johnjairo.restrepo@globant.com>
2022-02-22 21:35:26 -0500

    carpertaTopic

---
+++ Topic4/src/test/java/com/example/Topic4/Topic4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Topic4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Topic4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
